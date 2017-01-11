81f925b8cb38016c906d14b0504c2b7eb683a161
Hiro <hiro@inkri.com>
2017-01-11 10:56:10 +0000

    DIVER14-2 KADAI-a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs, only: [:index]
-  resources :poems, only: [:index]
+  #resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
 end
